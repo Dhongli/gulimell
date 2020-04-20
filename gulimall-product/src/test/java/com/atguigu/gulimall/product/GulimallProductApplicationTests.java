1c231dfb8e45f1ba3e8a64bda41476a544eace48
Dhongli <1215765236@qq.com>
2020-04-20 23:10:57 +0800

    19节,搭建好所有微服务框架及配置

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,29 @@
 package com.atguigu.gulimall.product;
 
+import com.atguigu.gulimall.product.entity.BrandEntity;
+import com.atguigu.gulimall.product.service.BrandService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
 
+    @Autowired
+    BrandService brandService;
+
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+//        brandEntity.setDescript("哈哈哈");
+//        brandEntity.setName("一加10pro");
+//        brandService.save(brandEntity);
+        List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 3L));
+        list.forEach((item)-> System.out.println(item));
+
     }
 
 }
